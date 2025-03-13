1ebd8f20be755b993081cac67264e24b1913bd70
Cleonis <cleonis-cls@outlook.com>
2025-03-13 00:25:35 +0000

    Tela inicial (home) criada na feature/cleonis

--- app/src/main/java/com/example/gamepocket/core/ui/adapter/GameAdapter.kt
+++ app/src/main/java/com/example/gamepocket/core/ui/adapter/GameAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.gamepocket.core.ui.adapter
+package package com.example.gamepocket.core.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -6,7 +6,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.gamepocket.core.data.model.Game
 import com.example.gamepocket.databinding.ItemGameBinding // Import correto
-import com.example.gamepocket.databinding.ItemHomeBinding
 
 // Adaptador para a lista de jogos
 class GameAdapter(
@@ -16,32 +15,26 @@ class GameAdapter(
 
     private val games = mutableListOf<Game>()
 
-
     fun submitList(newList: List<Game>) {
         games.clear()
         games.addAll(newList)
         notifyDataSetChanged()
     }
 
-
     // Inflar o layout do item da lista
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)=
-      GameViewHolder(ItemGameBinding.inflate(LayoutInflater.from(parent.context), parent, false))
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        GameViewHolder(ItemGameBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
     override fun onBindViewHolder(holder: GameViewHolder, position: Int) = holder.bind(games[position])
 
     override fun getItemCount() = games.size
 
- // ViewHolder para cada item da lista de jogos
+    // ViewHolder para cada item da lista de jogos
     inner class GameViewHolder(private val binding: ItemGameBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(game: Game) {
             binding.gameTitle.text = game.name
             Glide.with(binding.gameImage.context).load(game.backgroundImage).into(binding.gameImage)
             binding.root.setOnClickListener { onClick(game) }
-
         }
     }
 }
-
-
